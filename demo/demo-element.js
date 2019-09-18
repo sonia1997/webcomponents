7c0671ab4928a38a4773ab573c5d0916997fc5fc
sonia1997 <30790990+sonia1997@users.noreply.github.com>
2019-09-18 11:31:28 +0530

    demo-element modified

--- demo/demo-element.js
+++ demo/demo-element.js
@@ -1,5 +1,5 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import '@polymer/iron-icons/iron-icons.js';
+import 'https://unpkg.com/tonysoft@1.53.21/iron-icons.js';
 import '../icon-toggle.js';
 
 class DemoElement extends PolymerElement {
@@ -8,17 +8,23 @@ class DemoElement extends PolymerElement {
       <style>
         :host {
           font-family: sans-serif;
+          --icon-toggle-color: lightgrey;
+          --icon-toggle-outline-color: black;
+          --icon-toggle-pressed-color: yellow;
         }
       </style>
 
 
       <center>
       <h3>Simple rating bar Web component</h3>
+      <icon-toggle toggle-icon="star" pressed></icon-toggle>
       <icon-toggle toggle-icon="star"></icon-toggle>
       <icon-toggle toggle-icon="star"></icon-toggle>
       <icon-toggle toggle-icon="star"></icon-toggle>
       <icon-toggle toggle-icon="star"></icon-toggle>
-      <icon-toggle toggle-icon="star"></icon-toggle>
+      <!-- <icon-toggle toggle-icon="add"></icon-toggle> -->
+
+      <icon-toggle thing-count=5></icon-toggle>
       </center>
 
 
@@ -39,9 +45,11 @@ class DemoElement extends PolymerElement {
   }
   _message(fav) {
     if (fav) {
+      console.log("Favourite: "+ fav )
       return 'You really like me!';
     }
     else {
+      console.log("Favourite: "+ fav)
       return 'Do you like me?';
     }
   }
